49f248a224e949ee896b1c37223b85a66f69f989
JBrooksCS <jasonbrooksbio@gmail.com>
2019-09-14 11:10:48 -0500

    Event photos are working. Will probably need to check file type later.

---
+++ LocalShowsOnly/Migrations/20190913203317_removedReqdFromEventLink.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalShowsOnly.Migrations
+{
+    public partial class removedReqdFromEventLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "title",
+                table: "Event",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 40);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "externalLink",
+                table: "Event",
+                maxLength: 80,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 80);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "title",
+                table: "Event",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "externalLink",
+                table: "Event",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 80,
+                oldNullable: true);
+        }
+    }
+}
